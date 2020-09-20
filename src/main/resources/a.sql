3df8e4480b3a5c0fbc9fa1d468af926009066e67
gitofzbf <gitofzbf@email.com>
2020-09-21 02:06:18 +0800

    备份表数据

---
+++ src/main/resources/a.sql
@@ -0,0 +1,68 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.27 : Database - manager
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`manager` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `manager`;
+
+/*Table structure for table `school_class` */
+
+DROP TABLE IF EXISTS `school_class`;
+
+CREATE TABLE `school_class` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_name` varchar(255) DEFAULT NULL COMMENT '班级名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `school_class` */
+
+insert  into `school_class`(`id`,`class_name`) values (1,'一年一班'),(2,'一年二班'),(3,'一年三班');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  `school_class_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`age`,`teacher_id`,`school_class_id`) values (1,'zhangsan',12,1,2),(2,'lisi',21,3,2),(3,'wangwu',12,1,3),(4,'zhaoliu',23,4,2),(5,'liuqi',23,3,1),(6,'zhangbingfan',26,3,2),(7,'huajiu',12,4,1),(8,'zhangfei',23,2,3),(9,'nodejs',12,2,3);
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `school_class_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`id`,`name`,`school_class_id`) values (1,'张老师','1'),(2,'李老师','3'),(3,'王老师','2'),(4,'刘老师','3');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
